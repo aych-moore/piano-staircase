5afb1887e929b898b37365eaf52cf2506d89e269
Hayden <htmoor@gmail.com>
2019-07-21 01:14:20 +1200

    audio test

--- piano_script.py
+++ piano_script.py
@@ -1,13 +1,21 @@
-import os
 import time
+import pygame
 
 print("script begin")
 
 letters = ['A', 'B', 'C', 'D', 'E', 'F']
 
+pygame.mixer.init()
+pygame.mixer.music.load("C5.wav")
+pygame.mixer.music.play()
+while pygame.mixer.music.get_busy() == True:
+    continue
+
+
+"""
 for letter in letters:
     for num in range(8):
         os.system('mpg321 audio/piano/' + letter + str(num) + '.mp3 &')
         time.sleep(200)
-
+"""
 print("script end")
