a4b0b6e2b9a01d710ac5b4b36464117e5c475f29
Hayden <htmoor@gmail.com>
2019-07-21 19:13:12 +1200

    audio test

--- piano_script.py
+++ piano_script.py
@@ -5,14 +5,15 @@ print("script begin")
 
 letters = ['A', 'B', 'C', 'D', 'E', 'F']
 
-
-
+pygame.mixer.init()
+pygame.init()
+pygame.mixer.set_num_channels(50)
 
 for letter in letters:
     for num in range(8):
         audio_file = "audio/piano/" + letter + str(num) + ".mp3"
         print("PLAYING: " + audio_file)
-        pygame.mixer.init()
+        
         pygame.mixer.music.load(audio_file)
         pygame.mixer.music.play()
         time.sleep(500)
