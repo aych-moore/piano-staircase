9cdd7c2931e5543d79e0d549cc82076905de3a23
Hayden <htmoor@gmail.com>
2019-08-13 04:02:16 +1200

    log file

--- piano_script3.py
+++ piano_script3.py
@@ -51,13 +51,11 @@ def log_interaction():
     global interaction_count
     interaction_count += 1
     print("LOG")
-    """
     if(interaction_count >= 2):
         f = open( '/home/pi/piano-staircase/interaction_log.txt', 'a')
         f.write("C")
         f.close()
         interaction_count = 0
-        """
 
 def play_drums(message_data):
     for i in range(1,9):
